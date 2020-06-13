148982cd79304bd072ab16b7f80f9d5c5697cf4b
Мария <slinkywizard@mail.ru>
2020-06-13 20:18:44 +0300

    Внесены изменения

--- js/main.js
+++ js/main.js
@@ -12,8 +12,10 @@ var MIN_QTY_ROOMS = 1;
 var MAX_QTY_ROOMS = 10;
 var MIN_QTY_GUEST = 1;
 var MAX_QTY_QUEST = 10;
-var PIN_X_GAP = 25; // ширина метки пополам для расчета координаты по X
-var PIN_Y_GAP = 70; // высота метки для раcчета координаты по Y
+var PIN_X = 25; // ширина метки пополам для расчета координаты по X
+var PIN_Y = 70; // высота метки для раcчета координаты по Y
+var MUFFIN_WIDTH = 62; // ширина красной метки
+var MUFFIN_HEIGHT = 84; // высота красной метки
 var WIDTH_BLOCK = 1100; // максимум по X где может находиться метка
 var PIN_Y_MIN = 130;
 var PIN_Y_MAX = 630;
@@ -30,28 +32,29 @@ var CAPACITY = {
   '100': 0
 };
 var MAX_PRICE = 1000000;
-var titleArray = ['Bungalos', 'Prince palace', 'Down town Appartments']; // строка, заголовок предложения
-var typeArray = ['palace', 'flat', 'house', 'bungalo']; // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-var checkinArray = ['12:00', '13:00', '14:00']; // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-var checkoutArray = ['12:00', '13:00', '14:00']; // трока с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-var descriptionArray = ['descrText1', 'descrText2', 'descrText3']; // строка с описанием,
-var photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var advertTitles = ['Bungalos', 'Prince palace', 'Down town Appartments']; // строка, заголовок предложения
+var housingTypes = ['palace', 'flat', 'house', 'bungalo']; // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+var checkinTimes = ['12:00', '13:00', '14:00']; // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+var checkoutTimes = ['12:00', '13:00', '14:00']; // трока с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+var housingFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+var advertDescriptions = ['descrText1', 'descrText2', 'descrText3']; // строка с описанием,
+var advertPictures = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var similarListElement = document.querySelector('.map__pins');
 var similarAdvertTemplate = document.querySelector('#pin');
 var newAdvert = similarAdvertTemplate.content.querySelector('.map__pin');
 var mapFiltersForm = document.querySelector('.map__filters');
 var adForm = document.querySelector('.ad-form');
-var fieldsetForms = adForm.getElementsByTagName('fieldset');
-var fieldsetFilters = mapFiltersForm.getElementsByTagName('fieldset');
-var selectFilters = mapFiltersForm.getElementsByTagName('select');
+var fieldsetForms = adForm.querySelector('.ad-form__element');
+var fieldsetFilters = mapFiltersForm.querySelector('.map__features');
+var selectFilters = mapFiltersForm.querySelector('.map__filter');
 var mapPinMain = document.querySelector('.map__pin--main');
 var titleInput = adForm.querySelector('.title');
 var pricePerNight = adForm.querySelector('.price');
 var typeOfHousing = adForm.querySelector('.housing_type');
 var qtyRooms = adForm.querySelector('.room_number');
 var qtyGuests = adForm.querySelector('.capacity');
+var addressInput = adForm.querySelectorAll('.address');
 // var submitButton = adForm.querySelector('.ad-form__submit');
 
 typeOfHousing.addEventListener('change', function () {
@@ -107,39 +110,61 @@ selectQtyRooms.addEventListener('invalid', function () {
   }
 });
 */
-mapPinMain.addEventListener('mousedown', lMouseButton);
+mapPinMain.addEventListener('mousedown', clickLeftMouseButton);
 
 var flagMapActivation = false;
 
-function lMouseButton(e) {
-  if (typeof e === 'object') {
-    if (e.button === 0) {
-      if (!flagMapActivation) {
-        activateMap();
-        flagMapActivation = true;
-      }
-
-      activateElementsOfForm(fieldsetForms);
-      activateElementsOfForm(fieldsetFilters);
-      activateElementsOfForm(selectFilters);
-
-      adForm.classList.remove('ad-form--disabled');
+function clickLeftMouseButton(e) {
+  if (typeof e === 'object' && e.button === 0) {
+    if (!flagMapActivation) {
+      activateMap();
+      flagMapActivation = true;
     }
+
+    activateElementsOfForm(fieldsetForms);
+    activateElementsOfForm(fieldsetFilters);
+    activateElementsOfForm(selectFilters);
+
+    adForm.classList.remove('ad-form--disabled');
+
+    var xCoordinate = mapPinMain.style.left;
+    var yCoordinate = mapPinMain.style.top;
+    addressInput.value = Number(xCoordinate.substr(0, xCoordinate.length-2)) + MUFFIN_WIDTH / 2 +', '+ Number(Number(yCoordinate.substr(0, yCoordinate.length-2)) + MUFFIN_HEIGHT);
+    console.log(addressInput.value);
+    //console.log(mapPinMain);
   }
 }
 // console.log(fieldsetForms);
-
+/*
+var calculateCoordinates = function() {
+  return
+}
+*/
 mapFiltersForm.classList.add('ad-form--disabled');
 
-var activateElementsOfForm = function (tagArray) {
-  for (var i = 0; i < tagArray.length; i++) {
-    tagArray[i].removeAttribute('disabled');
+/*
+var activateElementsOfForm = function (tags) {
+  tags.forEach(function (el) {
+    el.removeAttribute('disabled');
+  });
+};
+*/
+
+var activateElementsOfForm = function (tags) {
+  /*
+  for (var tag in tags) {
+    tag.removeAttribute('disabled');
+  }
+*/
+  for (var i = 0; i < tags.length; i++) {
+    tags[i].removeAttribute('disabled');
   }
 };
 
-var disactivate = function (tagArray) {
-  for (var i = 0; i < tagArray.length; i++) {
-    tagArray[i].setAttribute('disabled', true);
+
+var disactivate = function (tags) {
+  for (var i = 0; i < tags.length; i++) {
+    tags[i].setAttribute('disabled', true);
   }
 };
 
@@ -169,17 +194,17 @@ var getMocks = function (number) {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       }, // где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
       offer: {
-        title: getRandomElement(titleArray), // строка, заголовок предложения
+        title: getRandomElement(advertTitles), // строка, заголовок предложения
         address: getRandomInteger(LOC_X_MIN, LOC_X_MAX) + ', ' + getRandomInteger(LOC_Y_MIN, LOC_Y_MAX), // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
         price: getRandomInteger(MIN_PRICE, MAX_PRICE), // число, стоимость
-        type: getRandomElement(typeArray), // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+        type: getRandomElement(housingTypes), // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
         rooms: getRandomInteger(MIN_QTY_ROOMS, MAX_QTY_ROOMS), // число, количество комнат
         guests: getRandomInteger(MIN_QTY_GUEST, MAX_QTY_QUEST), // число, количество гостей, которое можно разместить
-        checkin: getRandomElement(checkinArray), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-        checkout: getRandomElement(checkoutArray), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-        features: getRandomElement(featuresArray), // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-        description: getRandomElement(descriptionArray), // строка с описанием,
-        photos: getRandomElement(photosArray) // массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+        checkin: getRandomElement(checkinTimes), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+        checkout: getRandomElement(checkoutTimes), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+        features: getRandomElement(housingFeatures), // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+        description: getRandomElement(advertDescriptions), // строка с описанием,
+        photos: getRandomElement(advertPictures) // массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
       },
       location: {
         x: getRandomInteger(X_SHIFT, WIDTH_BLOCK - 2 * X_SHIFT), // случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
@@ -196,8 +221,8 @@ var renderAdvert = function (variantStorage) {
   var advertElement = newAdvert.cloneNode(true);
   advertElement.children[0].src = variantStorage.author.avatar;
   advertElement.children[0].alt = variantStorage.offer.title;
-  advertElement.style.left = variantStorage.location.x - PIN_X_GAP + 'px';
-  advertElement.style.top = variantStorage.location.y - PIN_Y_GAP + 'px';
+  advertElement.style.left = variantStorage.location.x - PIN_X + 'px';
+  advertElement.style.top = variantStorage.location.y - PIN_Y + 'px';
 
   return advertElement;
 };
